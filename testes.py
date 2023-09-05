d7a0857627f7b2db93d0d9fec81462ffe07c16bf
Asdfghc <lucashmeng@gmail.com>
2023-09-05 16:43:31 -0300

    Progresso da primeira aula

--- testes.py
+++ testes.py
@@ -1,9 +1,9 @@
 # Arquivo de testes
-import RobPy
+import RobPy as rp
 import numpy as np
 
-a = np.asarray([1, 2, 3])
-b = np.asarray([4, 5, 6])
+a = rp.cria_vetor3([1, 2, 3])
+b = rp.cria_vetor3([4, 5, 6])
 
 if __name__ == '__main__':
-    print(a + b)
+    print(rp.norma_vetor(a))

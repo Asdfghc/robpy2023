9944b88d7bb860cac80718cfc802a987c5a8a742
Asdfghc <lucashmeng@gmail.com>
2023-09-19 16:16:10 -0300

    progresso da aula 2

--- testes.py
+++ testes.py
@@ -1,9 +1,20 @@
 # Arquivo de testes
 import RobPy as rp
 import numpy as np
+import matplotlib.pyplot as plot
+from mpl_toolkits.mplot3d import Axes3D
 
 a = rp.cria_vetor3([1, 2, 3])
 b = rp.cria_vetor3([4, 5, 6])
 
-if __name__ == '__main__':
-    print(rp.norma_vetor(a))
+
+
+fig = plot.figure()
+ax = Axes3D(fig)
+fig.add_axes(ax)
+rp.plota_vetor3(a,color='r')
+rp.plota_vetor3(b,vo=a,color='r')
+plot.xlabel('Eixo X')
+plot.ylabel('Eixo Y')
+ax.set_zlabel('Eixo Z')
+plot.show()
